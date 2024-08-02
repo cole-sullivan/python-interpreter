c42b611321f9130398f0cb02fae0e0490a5ccdb7
cole-sullivan <mail@colesullivan.us>
2024-08-02 14:47:40 -0500

    Chunks setup

---
+++ chunk.h
@@ -0,0 +1,21 @@
+#ifndef CHUNK_H
+#define CHUNK_H
+
+#include "common.h"
+
+typedef enum {
+	OP_RETURN,
+} OpCode;
+
+typedef struct {
+	int count;
+	int capacity;
+	uint8_t* code;
+	int* lines;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
+#endif
