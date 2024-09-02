ba4975d9dbcd6a0f38bd240db549d4e0a503ddb4
cole-sullivan <mail@colesullivan.us>
2024-09-01 19:33:22 -0500

    Work on functions

---
+++ tests/scopes.py
@@ -0,0 +1,20 @@
+a = "A"
+    b = "B"
+        c = "C"
+        a
+        b
+        c
+            d = "D"
+            e = "E"
+                f = "F"
+                a
+                b
+                c
+                d
+                e
+                f
+            e
+            d
+        c
+    b
+a
